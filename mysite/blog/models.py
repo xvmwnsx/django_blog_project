eda2642dba1c339e093b307fa8d6d00c89417ebb
ksenia <kochetkovaksenia1@gmail.com>
2025-01-02 17:18:57 +0300

    добавлены представления для постов и ленты, обновлены модели

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+        
 class Post(models.Model):
     
     class Status(models.TextChoices):
@@ -9,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -17,9 +23,17 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     
+    #Поля модели
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() # конкретно-прикладной менеджер
+    
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish']),]
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.id])
