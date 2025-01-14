ec8bc22f1b1191eb1ddd4954e461cf304c53bf8f
ksenia <kochetkovaksenia1@gmail.com>
2025-01-14 14:21:11 +0300

    добавлена функция предложения похожих постов при помощи тегов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,8 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 
 @require_POST
 def post_comment(request, post_id):
@@ -61,9 +63,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+    
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
+    
     return render(request, 'blog/post/detail.html', {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form, 'similar_posts': similar_posts})
+    
+    
  
 def post_share(request, post_id):
     post = get_object_or_404(Post,
