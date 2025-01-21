aadf22ea22e167fca8f0806fd77072f4adea858f
ksenia <kochetkovaksenia1@gmail.com>
2025-01-21 12:46:29 +0300

    добавлена лента RSS постов

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,18 @@
+import markdown
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
+from django.urls import reverse_lazy
+from .models import Post
+ 
+class LatestPostsFeed(Feed):
+    title = 'Мой блог'
+    link = reverse_lazy('blog:post_list')
+    description = 'Новые посты моего блога'
+    def items(self):
+        return Post.published.all()[:5]
+    def item_title(self, item):
+        return item.title
+    def item_description(self, item):
+        return truncatewords_html(markdown.markdown(item.body), 30)
+    def item_pubdate(self, item):
+        return item.publish
